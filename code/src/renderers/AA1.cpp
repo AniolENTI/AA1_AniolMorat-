214392eab41fd70d541cc4d3c2e36fd2ae9ef637
Aniol Morató <aniol.morato@enti.cat>
2023-04-08 17:07:06 +0200

    Projecte Netejat

--- code/src/renderers/AA1.cpp
+++ code/src/renderers/AA1.cpp
@@ -8,23 +8,16 @@ AA1::AA1(int width, int height) : Renderer(width, height)
 	cube = new Cube();
 	objMat = glm::translate(glm::mat4(), glm::vec3(0.0f, 0.0f, -10.0f));
 	color = { 1.0f, 0.3f, 0.6f, 1.0f };
-
-	lightColor = { 0.5f, 0.3f, 0.8f };
-	lightPos = { 1.0f, 1.0f, 0.6f };
-	light = new Light(lightColor, 0.1f, lightPos);
-	light->SetAmbientResult(color);
 }
 
 AA1::~AA1()
 {
 	delete cube;
-	delete light;
 }
 
 void AA1::render(float dt)
 {
-	glm::vec4 aux = { light->GetAmbientResult(), 1 };
-	cube->setColor(aux);
+	cube->setColor(color);
 	cube->setTransforms(objMat, cam);
 	cube->draw();
 }
